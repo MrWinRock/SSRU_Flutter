34499d54f2143b5e1aef09c055ad2f6dcebc38bf
MrWinRock <mrwinrock11@gmail.com>
2023-05-04 13:37:50 +0700

    Edit todo_app elements, data

--- todo_app/lib/data/task_list.dart
+++ todo_app/lib/data/task_list.dart
@@ -1,7 +1,34 @@
 import '../models/task.dart';
 
 List<Task> taskList = [
-  Task(id: 0, body: 'Teaching states in flutter', status: Status.process),
-  Task(id: 1, body: 'Implementing user authentication', status: Status.process),
-  Task(id: 2, body: 'Creating a responsive UI design', status: Status.process),
+  Task(
+      id: 0,
+      body: 'Teaching states in flutter',
+      status: Status.process,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
+  Task(
+      id: 1,
+      body: 'Implementing user authentication',
+      status: Status.done,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
+  Task(
+      id: 2,
+      body: 'Creating a responsive UI design',
+      status: Status.done,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
+  Task(
+      id: 2,
+      body: 'Testing app on multiple devices',
+      status: Status.process,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
+  Task(
+      id: 2,
+      body: 'Preparing for presentation',
+      status: Status.process,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
 ];
